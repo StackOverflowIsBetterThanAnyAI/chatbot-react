457d513e4e99bf0a10c35fbff8f8ea19e400563b
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2024-11-28 09:43:43 +0100

    set font size

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -21,7 +21,9 @@ const ChatContainer = () => {
         <div className="w-full max-w-lg min-h-96 h-96 flex flex-col bg-gradient-to-b from-zinc-200 to-zinc-300 rounded-t-xl">
             <div className=" w-full flex justify-center items-center self-start gap-2 p-2 bg-gradient-to-b from-zinc-300 to-zinc-400 rounded-t-xl">
                 <Avatar isBot isHeading />
-                <span className="font-semibold">AI ChatBot</span>
+                <span className="font-semibold text-base sm:text-lg">
+                    AI ChatBot
+                </span>
             </div>
             <div
                 className="flex flex-col gap-2 p-2 overflow-y-auto scroll-smooth"
